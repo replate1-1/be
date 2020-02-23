4ebb6f9d9e5e974e82e7f03039abd5ea7d7321ca
lee-wood <leemaewood@gmail.com>
2020-02-23 02:03:40 -0500

    first test round written, half running red

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,4 +5,17 @@ describe('server.js', () => {
   it('sets the environment to testing', () => {
     expect(process.env.DB_ENV).toBe('testing');
   });
+
+  describe('/GET', () => {
+    it('connects and returns a 200 status', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    });
+    it('should return success message', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ message: "Server up and running!"});
+    });
+  });
 });
+
+//TODO - actually write the server code to make these run green.
