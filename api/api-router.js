3492993ba235add9c9cbd145dd5c58d01762a6e0
lee-wood <leemaewood@gmail.com>
2020-02-27 21:52:10 -0500

    user router endpoint channged from users to user

--- api/api-router.js
+++ api/api-router.js
@@ -3,7 +3,7 @@ const userRouter = require('../users/user-router.js');
 const authRouter = require('../auth/auth-router.js');
 
 router.use('/', authRouter);
-router.use('/users', userRouter);
+router.use('/user', userRouter);
 
 
 //* this is from the server file, things need to be switched over possibly same with the tests.
