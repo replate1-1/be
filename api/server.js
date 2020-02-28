f9eb3a5203ce0107b879e91b2bbe2248b45c2fd0
lee-wood <leemaewood@gmail.com>
2020-02-27 21:34:05 -0500

    comment cleanup

--- api/server.js
+++ api/server.js
@@ -43,5 +43,3 @@ server.use(session(sessionConfig));
 server.use('/api', apiRouter);
 
 module.exports = server;
-
-//TODO: transfer endpoints over to api-router...first re-write tests to run with that file instead of this one. Maybe we won't have to once the server is hooked up to the api-router because it will automatically redirect all requests to it? I'll have to see.
