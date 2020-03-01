00768ed04a22e6dd4a5afd022eda70fb388b7990
lee-wood <leemaewood@gmail.com>
2020-03-01 01:21:31 -0500

    added facility data to seed files

--- database/seeds/01-facilities.js
+++ database/seeds/01-facilities.js
@@ -1,13 +1,40 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  
+  return knex('facilities').truncate()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      
+      return knex('facilities').insert([
+        {
+          facilityName: "Martin de Porres House of Hospitality",
+          facilityAddress: "225 Potero Ave, San Francisco, CA 94103",
+          phoneNumber: "(415) 552-0240"
+        },
+        {
+          facilityName: "Mission Neighborhood Resource Center",
+          facilityAddress: "165 Capp St, San Francisco, CA 94110",
+          phoneNumber: "(415) 869-7977"
+        },
+        {
+          facilityName: "Navigation Center",
+          facilityAddress: "1950 Mission St, San Francisco, CA 94103",
+          phoneNumber: "(415) 487-3300"
+        },
+        {
+          facilityName: "Sanctuary Shelter",
+          facilityAddress: "201 8th St, San Francisco, CA 94103",
+          phoneNumber: "(415) 487-3300"
+        },
+        {
+          facilityName: "Raphael House",
+          facilityAddress: "1065 Sutter St, San Francisco, CA 94109",
+          phoneNumber: "(415) 345-7200"
+        },
+        {
+          facilityName: "Compass Family Services",
+          facilityAddress: "49 Powell St, San Francisco, CA 94102",
+          phoneNumber: "(415) 644-0504"
+        },
       ]);
     });
 };
