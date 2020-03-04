8587c61fd8ff5227474c7ca54dbe3954510baec2
lee-wood <leemaewood@gmail.com>
2020-03-03 21:34:31 -0500

    new migration and added old db to gitignore

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/replatedb.db3'
+      filename: './database/replate.db3'
     },
     useNullAsDefault: true,
     migrations: {
