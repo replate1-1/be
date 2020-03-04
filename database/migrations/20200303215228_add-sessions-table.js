4ada0a02e699f780f2bacb3cef92d9b5592da6e6
Lee Wood <50416443+LeeWood@users.noreply.github.com>
2020-03-03 22:26:50 -0500

    Update 20200303215228_add-sessions-table.js

--- database/migrations/20200303215228_add-sessions-table.js
+++ database/migrations/20200303215228_add-sessions-table.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   
   return knex.schema
     .createTable('sessions', tbl => {
-      tbl.increments('SID', 225);
+      tbl.increments('sid', 225);
       tbl.json('sess').notNullable();
       tbl.datetime('expired').notNullable();
     })
