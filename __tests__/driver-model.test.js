631cd1fb45b866508bd1b4b4a82810c5ed5c5026
lee-wood <leemaewood@gmail.com>
2020-03-10 17:01:23 -0400

    updated tests, all endpoints covered

--- __tests__/driver-model.test.js
+++ __tests__/driver-model.test.js
@@ -1,14 +1,16 @@
 const db = require('../database/dbConfig.js');
 const Drivers = require('../users/driver-model.js');
+const jwt = require('jsonwebtoken');
+const secrets = require('../auth/secrets.js');
 
 beforeEach(async () => {
   await db('drivers').truncate();
 });
 
-//POST driver user
+
 describe('driver user queries', () => {
 
-  describe('POST new driver user', () => {
+  describe('CREATE new driver user', () => {
 
     it('adds a new driver to the db', async () => {
       await Drivers.add({
@@ -48,4 +50,163 @@ describe('driver user queries', () => {
       expect(driver.username).toBe('zoomzoom3');
     })
   })
+
+  describe('POST to login existing driver user', () => {
+
+    it('returns a token on login', async () => {
+
+      function genToken(user) {
+   
+        const payload = {
+          userId: user.id,
+          username: user.username
+        };
+      
+        const options = {
+          expiresIn: '12h'
+        };
+      
+        return token = jwt.sign(payload, secrets.jwtSecret, options);
+      }
+
+      const loginData = {
+        id: 1, 
+        username: "dons", 
+        password: "password"
+      }
+
+      function login(userData) {
+        const token = genToken(userData)
+        return {
+          token: token,
+          username: userData.username
+        };
+      }
+
+      const loggedIn = login(loginData);
+  
+      expect(loggedIn.token)
+      expect(loggedIn.username).toBe("dons");
+    })
+  })
+
+  describe('READ queries', () => {
+
+    it('GETS all drivers in db', async () => {
+
+      await Drivers.add({
+        username: "driver1",
+        email: "email@gmail.com",
+        password: "password",
+        volunteerName: "Randy Travis",
+        phoneNumber: "1-800-ZOOM-ZOOM"
+      })
+      await Drivers.add({
+        username: "driver2",
+        email: "email2@gmail.com",
+        password: "password",
+        volunteerName: "Randy Orton",
+        phoneNumber: "1-800-ZOOMY-ZOOMY"
+      })
+
+      const driverList = await Drivers.find();
+
+      expect(driverList).toHaveLength(2);
+      expect(driverList[1].name).toBe("Randy Orton");
+      //volunteerName returns as 'name' in the driver-model
+    });
+
+    it('GETS specific driver data by id', async () => {
+      
+      await Drivers.add({
+        username: "driver1",
+        email: "email@gmail.com",
+        password: "password",
+        volunteerName: "Randy Travis",
+        phoneNumber: "1-800-ZOOM-ZOOM"
+      });
+      await Drivers.add({
+        username: "driver2",
+        email: "email2@gmail.com",
+        password: "password",
+        volunteerName: "Randy Orton",
+        phoneNumber: "1-800-ZOOMY-ZOOMY"
+      });
+
+      const driver = await Drivers.findById(1);
+
+      expect(driver.id).toBe(1);
+      expect(driver.id).not.toBe(2);
+      expect(driver.username).toBe("driver1");
+    });
+
+    it('gets driver data by any unique column in the table, in this case email', async () => {
+
+      await Drivers.add({
+        username: "driver1",
+        email: "email@gmail.com",
+        password: "password",
+        volunteerName: "Randy Travis",
+        phoneNumber: "1-800-ZOOM-ZOOM"
+      });
+      await Drivers.add({
+        username: "driver2",
+        email: "email2@gmail.com",
+        password: "password",
+        volunteerName: "Randy Orton",
+        phoneNumber: "1-800-ZOOMY-ZOOMY"
+      });
+      await Drivers.add({
+        username: "driver3",
+        email: "email3@gmail.com",
+        password: "password",
+        volunteerName: "Randy Newman",
+        phoneNumber: "1-800-ZOOMER-ZOOMER"
+      });
+
+      const email = "email@gmail.com";
+      const driver = await Drivers.findBy({email});
+
+      expect(driver.username).toBe("driver1");
+      expect(driver.id).toBe(1);
+    })
+  })
+
+  describe('DELETE queries', () => {
+
+    it('removes driver user from db', async () => {
+
+      await Drivers.add({
+        username: "driver1",
+        email: "email@gmail.com",
+        password: "password",
+        volunteerName: "Randy Travis",
+        phoneNumber: "1-800-ZOOM-ZOOM"
+      });
+      await Drivers.add({
+        username: "driver2",
+        email: "email2@gmail.com",
+        password: "password",
+        volunteerName: "Randy Orton",
+        phoneNumber: "1-800-ZOOMY-ZOOMY"
+      });
+      await Drivers.add({
+        username: "driver3",
+        email: "email3@gmail.com",
+        password: "password",
+        volunteerName: "Randy Newman",
+        phoneNumber: "1-800-ZOOMER-ZOOMER"
+      });
+      
+      //removing users
+      await Drivers.remove(1);
+      await Drivers.remove(2);
+      await Drivers.remove(3);
+
+      const driverList = await db('drivers');
+
+      expect(driverList).toHaveLength(0);
+    })
+  })
+  
 })
--- __tests__/pickups-model.test.js
+++ __tests__/pickups-model.test.js
@@ -5,10 +5,12 @@ beforeEach(async () => {
   await db('pickups').truncate();
 });
 
-describe('pickups queries', () => {
+describe('CREATE queries', () => {
 
-  describe('POST new pickup', () => {
+  describe('POST new pickup (business)', () => {
+
     it('adds a new pickup to the db in pickups table', async () => {
+
       await Pickups.addPickup({
         food: 'apples',
         amount: 55,
@@ -20,13 +22,37 @@ describe('pickups queries', () => {
         time: '00:22:00',
         date: '2020-03-12'
       })
+      await Pickups.addPickup({
+        food: 'rice',
+        amount: 10,
+        description: 'uncooked, brown',
+        dropOffId: 3,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business2',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'chicken',
+        amount: 3,
+        description: 'Full, oven roasted.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
       
-      const newPickup = await db('pickups')
-      expect(newPickup).toHaveLength(1);
+      const pickups = await db('pickups');
+
+      expect(pickups).toHaveLength(3);
     })
 
     it('should return new pickup data', async () => {
-      let pickup = await Pickups.addPickup({
+
+      await Pickups.addPickup({
         food: 'apples',
         amount: 55,
         description: 'golden delicious, will keep one month.',
@@ -37,7 +63,308 @@ describe('pickups queries', () => {
         time: '00:22:00',
         date: '2020-03-12'
       })
-      expect(pickup.food).toBe('apples');
+
+      const pickup = await db('pickups');
+
+      expect(pickup[0].food).toBe('apples');
+      expect(pickup[0].amount).toBe(55);
     })
+  })
+
+  describe('POST new driver accepted pickup', () => {
+
+    it('adds existing pickup to driver-pickups bridge table', async () => {
+
+      await Pickups.addPickup({
+        food: 'apples',
+        amount: 55,
+        description: 'golden delicious, will keep one month.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'rice',
+        amount: 10,
+        description: 'uncooked, brown',
+        dropOffId: 3,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business2',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'chicken',
+        amount: 3,
+        description: 'Full, oven roasted.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+  
+      await Pickups.addExistingPickup({
+        driverId: 1,
+        pickupId: 2
+      })
+  
+      const acceptedPickups = await db('driver-pickups');
+      
+      expect(acceptedPickups).toHaveLength(1);
+      expect(acceptedPickups[0].driverId).toBe(1);
+      expect(acceptedPickups[0].pickupId).toBe(2);
+    })
+
+  })
+});
+
+describe('READ queries', () => {
+
+  describe('GET all pickups in db', () => {
+
+    it('returns a list of all current pickups', async () => {
+
+      await Pickups.addPickup({
+        food: 'apples',
+        amount: 55,
+        description: 'golden delicious, will keep one month.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'rice',
+        amount: 10,
+        description: 'uncooked, brown',
+        dropOffId: 3,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business2',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+     
+      const pickups = await Pickups.findPickups();
+      
+      expect(pickups).toHaveLength(2);
+    })
+  })
+
+  describe('GET all pickups made by specific business, identified by business username', () => {
+
+    it('returns all records by business', async () => {
+
+      await Pickups.addPickup({
+        food: 'apples',
+        amount: 55,
+        description: 'golden delicious, will keep one month.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'rice',
+        amount: 10,
+        description: 'uncooked, brown',
+        dropOffId: 3,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business2',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'chicken',
+        amount: 3,
+        description: 'Full, oven roasted.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+
+      const businessPickups = await Pickups.findBusinessPickups('business');
+
+      expect(businessPickups).toHaveLength(2);
+      expect(businessPickups[1].food).toBe('chicken');
+    })
+  })
+
+  describe('GET a list of all driver-accepted data', () => {
+
+    it('returns the number of accpted pickups and the correct data', async () => {
+      //add pickups
+      await Pickups.addPickup({
+        food: 'apples',
+        amount: 55,
+        description: 'golden delicious, will keep one month.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      await Pickups.addPickup({
+        food: 'rice',
+        amount: 10,
+        description: 'uncooked, brown',
+        dropOffId: 3,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business2',
+        time: '00:22:00',
+        date: '2020-03-12' 
+      })
+      await Pickups.addPickup({
+        food: 'chicken',
+        amount: 3,
+        description: 'Full, oven roasted.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+      //accept driver pickups
+      await Pickups.addExistingPickup({
+        driverId: 1,
+        pickupId: 1
+      })
+      await Pickups.addExistingPickup({
+        driverId: 1,
+        pickupId: 3
+      })
+
+      const driverPickups = await Pickups.findAcceptedPickups(1);
+     
+      expect(driverPickups).toHaveLength(2);
+      expect(driverPickups[1].food).toBe('chicken');
+    })
+  })
+});
+
+describe('UPDATE queries', () => {
+
+  describe('edit existing pickup', () => {
+
+    it('edits specified pickups identified by pickup id', async () => {
+
+      await Pickups.addPickup({
+        food: 'apples',
+        amount: 55,
+        description: 'golden delicious, will keep one month.',
+        dropOffId: 2,
+        lat: 12.3334,
+        lng: -122.2222,
+        businessUsername: 'business',
+        time: '00:22:00',
+        date: '2020-03-12'
+      })
+
+      const changes = {
+        food: 'oranges',
+        amount: 54
+      }
+
+      await Pickups.updatePickup(changes, 1);
+
+      const pickup = await Pickups.findPickups();
+
+      expect(pickup[0].food).toBe('oranges');
+      expect(pickup[0].food).not.toBe('apples');
+      expect(pickup[0].amount).toBe(54);
+      expect(pickup[0].amount).not.toBe(55);
+    })
+  })
+})
+
+describe('DELETE queries', () => {
+
+  it('removes business pickup from pickups table', async () => {
+
+    await Pickups.addPickup({
+      food: 'apples',
+      amount: 55,
+      description: 'golden delicious, will keep one month.',
+      dropOffId: 2,
+      lat: 12.3334,
+      lng: -122.2222,
+      businessUsername: 'business',
+      time: '00:22:00',
+      date: '2020-03-12'
+    })
+    await Pickups.addPickup({
+      food: 'rice',
+      amount: 10,
+      description: 'uncooked, brown',
+      dropOffId: 3,
+      lat: 12.3334,
+      lng: -122.2222,
+      businessUsername: 'business2',
+      time: '00:22:00',
+      date: '2020-03-12' 
+    })
+    await Pickups.addPickup({
+      food: 'chicken',
+      amount: 3,
+      description: 'Full, oven roasted.',
+      dropOffId: 2,
+      lat: 12.3334,
+      lng: -122.2222,
+      businessUsername: 'business',
+      time: '00:22:00',
+      date: '2020-03-12'
+    })
+
+    await Pickups.removePickup(1);
+
+    const newPickupList = await Pickups.findPickups();
+
+    expect(newPickupList).toHaveLength(2);
+    expect(newPickupList[0].food).toBe('rice');
+
+  })
+
+  it('removes driver accepted pickups from the driver-pickups table', async () => {
+
+    await Pickups.addPickup({
+      food: 'apples',
+      amount: 55,
+      description: 'golden delicious, will keep one month.',
+      dropOffId: 2,
+      lat: 12.3334,
+      lng: -122.2222,
+      businessUsername: 'business',
+      time: '00:22:00',
+      date: '2020-03-12'
+    })
+
+    await Pickups.addExistingPickup({
+      driverId: 1,
+      pickupId: 1
+    })
+    
+    await Pickups.removeDriverPickup(1);
+    
+    const driverPickups = await Pickups.findAcceptedPickups(1);
+
+    expect(driverPickups).toHaveLength(0);
+    expect(driverPickups).not.toHaveLength(1);
   })
 });
